726d3af9025279f09983ad4cfeba92f0e6c5363a
Obaid Ullah <obaidsafi31@gmail.com>
2024-09-27 12:14:53 +0500

    QuickUnion lazy Algorithm

---
+++ QuickUnion.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class QuickUnion {
+    private int[] id;
+
+    public QuickUnion(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }// end of for
+    }// end of quickUnion
+
+    public int root(int p) {
+        while (p != id[p]) p = id[p];
+        return p;
+    }
+    public void connected(int p, int q) { // Tells whether the nodes are connected or not
+        if(root(p) == root(q)) StdOut.print("Connected");
+        else StdOut.print("Not Connected");
+    }
+    public void union(int p, int q ){
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+    public static void main(String[] args) {
+        QuickUnion qu = new QuickUnion(100000);
+       qu.union(0,1000);
+        qu.connected(1,1000);
+    }
+}
+
+
