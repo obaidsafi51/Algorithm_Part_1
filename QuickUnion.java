939eacdc19ffc44cb547024fe9e1a7bf22ef5a8c
Obaid Ullah <obaidsafi31@gmail.com>
2024-09-27 12:16:54 +0500

    Comments added

--- QuickUnion.java
+++ QuickUnion.java
@@ -13,22 +13,24 @@ public class QuickUnion {
     public int root(int p) {
         while (p != id[p]) p = id[p];
         return p;
-    }
+    }// end of root
+
     public void connected(int p, int q) { // Tells whether the nodes are connected or not
         if(root(p) == root(q)) StdOut.print("Connected");
         else StdOut.print("Not Connected");
-    }
+    }// end of connected
+
     public void union(int p, int q ){
         int i = root(p);
         int j = root(q);
         id[i] = j;
-    }
+    }// end of union
 
     public static void main(String[] args) {
         QuickUnion qu = new QuickUnion(100000);
        qu.union(0,1000);
         qu.connected(1,1000);
-    }
-}
+    }// end of main
+}// end of quickunion
 
 
